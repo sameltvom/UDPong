ed390ece780227d458efeb3be0729a7774cbb6f3
Samuel Skanberg <dt05ss5@student.lth.se>
2010-03-05 20:27:45 +0100

    Wohoo, MainGUI

--- src/main/UDPongMain.java
+++ src/main/UDPongMain.java
@@ -1,7 +1,10 @@
 package main;
 
+import view.MainGUI;
+
 public class UDPongMain {
 	public static void main(String []args) {
+		MainGUI gui = new MainGUI();
 		System.out.println("Hello UDPong!");
 	}
 }
---
+++ src/view/MainGUI.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class MainGUI extends JFrame {
+	public MainGUI() {
+		this.setVisible(true);
+		
+	}
+}
