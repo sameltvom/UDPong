0aa88df40722cfd934990af4a67a2ba75929dac9
Samuel Skanberg <dt05ss5@student.lth.se>
2010-03-05 20:36:57 +0100

    Hello is a word that now is shown

--- src/view/DrawingArea.java
+++ src/view/DrawingArea.java
@@ -1,11 +1,13 @@
 package view;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class DrawingArea extends JPanel {
 	private int BufferedImage;
 	public DrawingArea() {
-		
+		this.add(new JLabel("Hello"));
+		this.setVisible(true);
 	}
 
 }
--- src/view/MainGUI.java
+++ src/view/MainGUI.java
@@ -4,7 +4,10 @@ import javax.swing.JFrame;
 
 public class MainGUI extends JFrame {
 	public MainGUI() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(100, 100);
+		this.add(new DrawingArea());
+		this.pack();
 		this.setVisible(true);
-		
 	}
 }
