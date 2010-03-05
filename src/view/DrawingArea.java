e0128987920c74dd2148c730f0e4e1caeeeb6e2f
Samuel Skanberg <dt05ss5@student.lth.se>
2010-03-05 21:04:44 +0100

    Now we can draw stuff

--- src/view/DrawingArea.java
+++ src/view/DrawingArea.java
@@ -1,13 +1,40 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class DrawingArea extends JPanel {
-	private int BufferedImage;
+public class DrawingArea extends Component {
+	private BufferedImage img;
+	
 	public DrawingArea() {
-		this.add(new JLabel("Hello"));
+		/* Create something to draw on */
+		img = new BufferedImage(640, 480, BufferedImage.TYPE_BYTE_INDEXED);
+		Graphics2D g = (Graphics2D)img.getGraphics();
+		g.setColor(Color.BLUE);
+		g.drawLine(10, 10, 20, 20);
+		
 		this.setVisible(true);
 	}
 
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+    }
+
+    public Dimension getPreferredSize() {
+        if (img == null) {
+             return new Dimension(100,100);
+        } else {
+           return new Dimension(img.getWidth(null), img.getHeight(null));
+       }
+    }
 }
