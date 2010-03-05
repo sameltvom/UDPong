06b46fc8d9b4fa3431390cd6abb2f0762f2d8b45
Samuel Skanberg <dt05ss5@student.lth.se>
2010-03-05 22:31:29 +0100

    Added comment

--- src/view/DrawingArea.java
+++ src/view/DrawingArea.java
@@ -39,6 +39,7 @@ public class DrawingArea extends Component {
 	public void drawBall(int x, int y) {
 		Graphics2D g = (Graphics2D)img.getGraphics();
 		g.drawImage(ballSprite, x, y, null);
+		// do the repainting stuff
 		repaint();
 	}
 
